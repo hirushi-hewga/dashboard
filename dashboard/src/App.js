7bec527c71e669639557d0b25d81f0e62ee3816e
KuryloBohdan <149187276+hirushi-hewga@users.noreply.github.com>
2025-02-09 15:19:43 +0200

    Update App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,12 +1,39 @@
-import './App.css'
+import { useEffect, useState } from 'react'
 import MainPage from './pages/mainPage/MainPage'
 import Footer from './components/footer'
 import Navbar from './components/navbar/Navbar'
+import './App.css'
 
 const App = () => {
+    // state
+    const [theme, setTheme] = useState("light")
+
+
+    const ChangeThemeHandler = () => {
+        const currentTheme = theme === "dark" ? "light" : "dark"
+        setTheme(currentTheme)
+        localStorage.setItem("theme", currentTheme)
+        console.log("event theme", currentTheme)
+    }
+
+
+    function getThemeCallBack() {
+        const currentTheme = localStorage.getItem("theme")
+        if (currentTheme) {
+            setTheme(currentTheme)
+        }
+    }
+  
+
+    useEffect(getThemeCallBack, [])
+
+
   return (
     <>
-      <Navbar />
+      <Navbar isDark={theme === "dark"} />
+      <div>
+        <button onClick={ChangeThemeHandler} style={{margin: "10px 5px"}}>Change theme</button>
+      </div>
       <MainPage />
       <Footer />
     </>

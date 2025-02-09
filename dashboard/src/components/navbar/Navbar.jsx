b3223742dfb01634644c60013fc8f2bd78a476ea
KuryloBohdan <149187276+hirushi-hewga@users.noreply.github.com>
2025-02-09 15:22:39 +0200

    Update Navbar.jsx

--- dashboard/src/components/navbar/Navbar.jsx
+++ dashboard/src/components/navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import { SolidButton } from '../../Buttons'
+import * as style from './style'
 import './style.css'
 
-const Navbar = () => {
+const Navbar = ({isDark = false}) => {
+    const navLinkStyle = isDark ? style.navLinkDark : style.navLinkLight
+
     return (
-        <div className="navbar">
-            <a href="#">Main page</a>
-            <a href="#">About</a>
-            <a href="#">Page 3</a>
-            <a href="#">Page 4</a>
+        <div className='navbar' style={isDark ? style.navbarDark : style.navbarLight}>
+            <a className='navLink' style={navLinkStyle} href="#">Main page</a>
+            <a className='navLink' style={navLinkStyle} href="#">About</a>
+            <a className='navLink' style={navLinkStyle} href="#">Page 3</a>
+            <a className='navLink' style={navLinkStyle} href="#">Page 4</a>
             <SolidButton text="Page 5" color="green" />
         </div>
     )
